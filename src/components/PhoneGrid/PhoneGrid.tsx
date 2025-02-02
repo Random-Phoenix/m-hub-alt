6c00afc312fa79ff4c59410affcf72ef0be67e13
zaimayy <abbasizaim806@gmail.com>
2025-02-02 14:47:49 +0500

    remove unused useState import from PhoneGrid component

--- src/components/PhoneGrid/PhoneGrid.tsx
+++ src/components/PhoneGrid/PhoneGrid.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, memo, useCallback, useState } from "react";
+import React, { useMemo, memo, useCallback } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { PhoneCard } from "../PhoneCard/PhoneCard";
 import { Phone } from "../../types";
