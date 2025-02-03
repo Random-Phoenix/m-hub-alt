8bdcfd735180a5f510ca613be682511862272bdc
zaimayy <abbasizaim806@gmail.com>
2025-02-03 19:35:03 +0500

    update slide images for enhanced loading performance and visual quality

--- src/data/slides.ts
+++ src/data/slides.ts
@@ -2,32 +2,32 @@ import { Slide } from '../types';
 
 export const slides: Slide[] = [
   {
-    image: "https://images.unsplash.com/photo-1583573636246-18cb2246697f?q=50&w=800&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    image: "https://images.unsplash.com/photo-1583573636246-18cb2246697f?q=80&w=3438&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     title: "Samsung Galaxy S20",
     subtitle: "8K Video Snap, 5G connectivity, Space Zoom up to 100x."
   },
   {
-    image: "https://images.unsplash.com/photo-1655803243218-809254fd52fd?q=50&w=800&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    image: "https://images.unsplash.com/photo-1655803243218-809254fd52fd?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     title: "Oppo Find N3",
     subtitle: "Ultra Thin, Ultra Light, Ultra Powerful."
   },
   {
-    image: "https://images.unsplash.com/photo-1687168593635-370120784edc?q=50&w=800&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    image: "https://images.unsplash.com/photo-1687168593635-370120784edc?q=80&w=3269&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     title: "Vivo X Flip 2023",
     subtitle: "Light, Stylish, and Powerful"
   },
   {
-    image: "https://images.unsplash.com/photo-1727941035071-910fd07135bb?q=50&w=800&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    image: "https://images.unsplash.com/photo-1727941035071-910fd07135bb?q=80&w=3352&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     title: "Pixel 9 Pro",
     subtitle: "All pro, Pure magic."
   },
   {
-    image: "https://images.unsplash.com/photo-1610945263963-47c586d7abbf?q=50&w=800&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    image: "https://images.unsplash.com/photo-1610945263963-47c586d7abbf?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     title: "Samsung S21 Ultra",
     subtitle: "Professional photography capabilities in your pocket. "
   },
   {
-    image: "https://images.unsplash.com/photo-1621330396173-e41b1cafd17f?q=50&w=800&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    image: "https://images.unsplash.com/photo-1621330396173-e41b1cafd17f?q=80&w=3000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     title: "Xiaomi Mi 11",
     subtitle: "Quad-curved glass body."
   }
